9eda9d220b1b977f58cfc3b5be09580bdb68b034
Nishtha <nishtha.arora26@gmail.com>
2016-11-16 16:30:55 -0600

    changing port setting in server

--- server.js
+++ server.js
@@ -57,9 +57,13 @@ models.sequelize.sync();
 
 
 // // app listens on port 3000
-app.listen(3000, function() {
-	console.log("Listening on port 3000")
-})
+// app.listen(3000, function() {
+// 	console.log("Listening on port 3000")
+// })
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 // module.exports = app;
