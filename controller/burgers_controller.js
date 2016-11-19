54999cb631369716d7f875fd5c8da2fe1357c77e
Nishtha <nishtha.arora26@gmail.com>
2016-11-19 14:36:51 -0600

    adding comments

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -1,23 +1,21 @@
-// this is the cats_controller.js file
+// this is the burgers_controller.js file which is creatin gall the routes doe the users
 
 var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
+/* this is a home route this route is redirected to burgers route where all the
+ burgers will be displayed*/
 router.get('/', function(req, res) {
   res.redirect('/burgers')
 })
 
+// this route dispays all the burgers
+
 router.get('/burgers', function(req, res) {
   models.burgers.findAll({
     attributes: ['id', 'burger_name', 'devoured'],
-    // include: [models.User]
-
   }).then(function(result) {
-    // console.log('result');
-    // var allBurgers = {
-    //   burgers: result
-    // }
     res.render('burgers/index', {
       user_id: req.session.user_id,
       email: req.session.user_email,
@@ -27,6 +25,7 @@ router.get('/burgers', function(req, res) {
   })
 })
 
+// this is the route which is used to add a burger if user wants to
 router.post('/burgers/add', function(req, res) {
   console.log('add', req.body.name);
   return models.burgers.create({
@@ -36,6 +35,9 @@ router.post('/burgers/add', function(req, res) {
   })
 })
 
+/* this is a route which when user click on devoured button placed the burger in the
+in the devoured section
+*/
 router.put('/burgers/update/:id', function(req, res) {
 
     if(req.session.logged_in === true) {
@@ -56,5 +58,5 @@ router.put('/burgers/update/:id', function(req, res) {
   }
 })
 
-
+// exporting router will export all the routes to the server file
 module.exports = router
--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,23 +1,34 @@
+// this file is for user lofin and password
 var bcrypt = require('bcryptjs');
 var express = require('express');
 var models = require('../models');
 var router = express.Router();
 
-
+/* this route will render a page which lets user signup for the app.
+user cannot use the devoured button unless he logs into website
+*/
 router.get('/new', function(req, res) {
 	res.render('users/new');
 });
 
+
+// this route is for existing users to signin if the user already exist
 router.get('/sign-in', function(req, res) {
 	res.render('users/sign-in');
 });
 
+// once the user has signed in this route will let the user signout from the app
 router.get('/sign-out', function(req, res) {
 	req.session.destroy(function(err) {
 		res.redirect('/');
 	})
 });
 
+/*
+when user tries to login this function will check if the user exist or not.
+if the user doesnot exist then user will be asked to signup if the user exist
+this will let the user login and will compare the user password
+*/
 router.post('/login', function(req, res) {
 	models.User.findOne({
 		where: {
@@ -50,6 +61,11 @@ router.post('/login', function(req, res) {
 	})
 })
 
+
+/*
+this will check for the existing emails when the user tries to sign-up, if the email
+used by the user it will give the user a message saying this exist else it lets user signup
+*/
 router.post('/create', function(req, res) {
 	models.User.findAll({
 		where: {
